8390f2237fc703dadb61951c1ebdc36930e7f17c
samsondsouza <samsondsouza666@gmail.com>
2024-06-21 10:33:47 +0530

    add express chapter

---
+++ 02_express_tutorial/index.js
@@ -0,0 +1,73 @@
+import express from 'express'
+
+
+const port = 3000
+const  app = express()
+
+
+app.use(express.json())
+
+const tea = []
+let nextId =1;
+app.post("/tea",(req,res)=>{
+    // console.log(req.body)
+    const temp = req.body
+    
+    temp.forEach(element => {
+        // console.log(element)
+        const {name,price} = element
+        tea.push({id:nextId++,name,price})
+    });
+
+ 
+    res.send("data stored successfully")
+})
+app.get("/",(req,res)=>{
+    send("hello worllddd!!!")
+})
+app.get("/getTeas",(req,res)=>{
+    
+    res.status(201).send(tea)
+})
+
+app.get("/:id",(req,res)=>{
+    
+    const queryId = req.params.id
+    // console.log(queryId)
+    let flag = 0
+    tea.forEach(element =>{
+        if(element.id === parseInt(queryId))
+            res.send(element),flag=1
+    })
+    if(!flag) res.status(404).send("query data is not present")
+})
+
+app.put("/:id",(req,res)=>{
+    const queryId = parseInt(req.params.id)
+    let flag = 0
+    tea.forEach((element,index) =>{
+        if(element.id === parseInt(queryId))
+            tea[index] = req.body,flag=1,res.status(200).send(tea[index])
+
+        // console.log(index)
+    })
+    if(!flag) res.status(404).send("query data is not present")
+    
+})
+
+app.delete("/:id",(req,res)=>{
+    const queryId = parseInt(req.params.id)
+    let flag = 0
+    tea.forEach((element,index) =>{
+        if(element.id === parseInt(queryId))
+            tea.splice(index,1),flag=1,res.status(200).send(element)
+
+        // console.log(index)
+    })
+    if(!flag) res.status(404).send("query data is not present")
+    
+})
+
+app.listen(port,()=>{
+    console.log("listenning ........")
+})
