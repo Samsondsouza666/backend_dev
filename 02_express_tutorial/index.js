ca6f628e13050e79d977f78bb1ca904765d3a2c6
samsondsouza <samsondsouza666@gmail.com>
2024-06-22 10:15:56 +0530

    logger

--- 02_express_tutorial/index.js
+++ 02_express_tutorial/index.js
@@ -1,11 +1,30 @@
 import 'dotenv/config'
+import logger from './logger.js';
+import morgan from 'morgan';
 import express from 'express'
 const port = process.env.PORT || 3000
 const  app = express()
 
+const morganFormat = ':method :url :status :response-time ms';
+
 
 app.use(express.json())
 
+
+app.use(morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+  
+        };
+        logger.info(JSON.stringify(logObject));
+      }
+    }
+  }));
 const tea = []
 let nextId =1;
 app.post("/tea",(req,res)=>{
